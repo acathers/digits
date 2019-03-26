b7a31880f3c637fae35dc7ca0d7c37b4ec58332f
acathers <37058209+acathers@users.noreply.github.com>
2019-03-25 23:03:36 -1000

    notes-1 finished in 37 minutes.

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Feed } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import Note from '/imports/ui/components/Note';
+import AddNote from '/imports/ui/components/AddNote';
 import { withRouter, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
@@ -20,6 +22,14 @@ class Contact extends React.Component {
           <Card.Content extra>
             <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
           </Card.Content>
+          <Card.Content extra>
+            <Feed>
+              {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
+            </Feed>
+          </Card.Content>
+          <Card.Content extra>
+            <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
+          </Card.Content>
         </Card>
 
     );
@@ -29,6 +39,7 @@ class Contact extends React.Component {
 /** Require a document to be passed to this component. */
 Contact.propTypes = {
   contact: PropTypes.object.isRequired,
+  notes: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
